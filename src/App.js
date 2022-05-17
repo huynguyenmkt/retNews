5abeeadbabe5aebf24284de77b0bcffc1dcc00ea
huynguyenmkt <nguyentahuy2013@gmail.com>
2022-05-17 22:19:38 +0700

    build basic project and create header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import productApi from './api/productApi';
 import './App.css';
+import Button from '@mui/material/Button';
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './pages/home/Home';
+import Header from './components/common/Header';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </div>
   );
 }
